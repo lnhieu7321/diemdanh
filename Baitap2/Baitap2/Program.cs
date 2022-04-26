290e08282e3c7fe543467db9ecd395be28c4020b
NgocHieu <lengochieu732001@gmail.com>
2022-04-26 08:11:20 +0700

    upload

--- Baitap2/Baitap2/Program.cs
+++ Baitap2/Baitap2/Program.cs
@@ -8,30 +8,30 @@ namespace Baitap2
 {
     internal class Program
     {
-        public class Animal
+        public class Animal_122
         {
 
-            public void Eat()
+            public void Eat_122()
             {
                 Console.WriteLine("ăn. . .");
             }
-            public void makeSound()
+            public void makeSound_122()
             {
                 Console.WriteLine("phát ra tiếng. . .");
             }
         }
-        class Cat : Animal
+        class Cat_122 : Animal_122
         {
-            public void Run()
+            public void Run_122()
             {
                 Console.WriteLine("Mèo đang chạy. . .");
             }
-            Animal animal = new Animal();
+            Animal_122 animal = new Animal_122();
 
         }
-        class Bird : Animal
+        class Bird_122 : Animal_122
         {
-            public void Fly()
+            public void Fly_122()
             {
                 Console.WriteLine("Chim đang bay. . .");
             }
@@ -40,12 +40,12 @@ namespace Baitap2
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Cat cat = new Cat();
-            cat.Run();
-            cat.Eat();
-            Bird bird = new Bird();
-            bird.Fly();
-            bird.makeSound();
+            Cat_122 cat = new Cat_122();
+            cat.Run_122();
+            cat.Eat_122();
+            Bird_122 bird = new Bird_122();
+            bird.Fly_122();
+            bird.makeSound_122();
             Console.ReadKey();
         }
     }
